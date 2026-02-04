fc62667c5874c9b65d2b19a68d6bd84a860e3d4e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-04 16:15:26 -0300

    teste

--- aula2.dart
+++ aula2.dart
@@ -1,9 +1,6 @@
 void main() {
   //cont e final ---- VAR
-  // const String nome = 'Toshi Ossada';
-  final String nome;
-
-  nome = "Toshi Ossada";
+  const String nome = 'Toshi Ossada';
 
   print(nome);
 }
