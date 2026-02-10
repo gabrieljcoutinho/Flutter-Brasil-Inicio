cc0d2122a6373d4f7b1aefcca18f1b57661a3b90
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 07:33:14 -0300

    swit

--- aula07.dart
+++ aula07.dart
@@ -2,21 +2,23 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  final int diaDasemana;
+  final int diaDaSemana;
 
   print('Digite o numero referente ao dia da semana');
 
-  diaDasemana = int.parse(stdin.readLineSync(encoding: utf8) ?? '0');
+  diaDaSemana = int.parse(stdin.readLineSync(encoding: utf8) ?? '0');
 
   var diaSemanaString = ' ';
 
-    diaSemanaString = switch(diaDaSemana){
+    diaSemanaString = switch (diaDaSemana){
           0 => 'Segunda',
+
+
     }
 
-  if (diaDaSemana.isEmpty) {
+  if (diaSemanaString.isEmpty) {
     print('Dia inválido');
   } else {
-    print('Olá, hoje é $diaDaSemana');
+    print('Olá, hoje é $diaSemanaString');
   }
 }
