2a90fb21f8611255f1d4d36937d9f611e168a070
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 07:23:34 -0300

    dias da semana

--- aula06.dart
+++ aula06.dart
@@ -10,13 +10,25 @@ void main() {
 
   switch (diaDasemana) {
     case 1:
-      print('Segunda');
+      print('Domingo');
       break;
     case 2:
-      print('Terça');
+      print('Segunda');
       break;
     case 3:
+      print('Terça');
+      break;
+    case 4:
       print('Quarta');
+      break;
+    case 5:
+      print('Quinta');
+      break;
+    case 6:
+      print('Sexta');
+      break;
+    case 7:
+      print('Sábado');
       break;
     default:
       print('dia invalido');
