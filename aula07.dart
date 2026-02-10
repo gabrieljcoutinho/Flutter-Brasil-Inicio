2551ff3d51ba17d4f91ec8efcb8fcb24cabcf26e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 07:31:43 -0300

    dia da semana

--- aula07.dart
+++ aula07.dart
@@ -8,30 +8,11 @@ void main() {
 
   diaDasemana = int.parse(stdin.readLineSync(encoding: utf8) ?? '0');
 
-  var diaDaSemana = ' ';
+  var diaSemanaString = ' ';
 
-  switch (diaDasemana) {
-    case 1:
-      diaDaSemana = 'Domingo';
-      break;
-    case 2:
-      diaDaSemana = "Segunda";
-      break;
-    case 3:
-      diaDaSemana = "Terça";
-      break;
-    case 4:
-      diaDaSemana = "Quarta";
-      break;
-    case 5:
-      diaDaSemana = "Quinta";
-      break;
-    case 6:
-      diaDaSemana = "Sexta";
-      break;
-    case 7:
-      diaDaSemana = "Sabado";
-  }
+    diaSemanaString = switch(diaDaSemana){
+          0 => 'Segunda',
+    }
 
   if (diaDaSemana.isEmpty) {
     print('Dia inválido');
