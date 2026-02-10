6e7b38b7b88a7977bbeec16e30a08f08615d3bb6
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 08:00:30 -0300

    aula o7

--- aula07.dart
+++ aula07.dart
@@ -4,17 +4,17 @@ import 'dart:io';
 void main() {
   final int diaDaSemana;
 
-  print('Digite o numero referente ao dia da semana');
+  print('Digite o numero referente ao dia da semana:');
 
   diaDaSemana = int.parse(stdin.readLineSync(encoding: utf8) ?? '0');
 
-  var diaSemanaString = ' ';
-
-    diaSemanaString = switch (diaDaSemana){
-          0 => 'Segunda',
-
-
-    }
+  // Ajuste aqui: use o _ como wildcard para o caso padrão
+  var diaSemanaString = switch (diaDaSemana) {
+    0 => 'Segunda',
+    1 => 'Terça',
+    2 => 'Quarta',
+    _ => '', // O '_' captura qualquer valor que não seja 0, 1 ou 2
+  };
 
   if (diaSemanaString.isEmpty) {
     print('Dia inválido');
